6a6c0284a5d93446103809d64f13595bbdcc1626
jpggonsales <jprock5000@gmail.com>
2025-12-05 00:28:17 +0000

    NIVEL AVENTUREIRO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,46 +1,58 @@
 #include <stdio.h>
 
-int main(){
-//Vetor unidimensional para os navios
-    int naviohori[3] = {3,3,3};   // Navio horizontal (3 células)
-    int naviovert[3] = {3,3,3};   // Navio vertical   (3 células)
-//Matriz para o tabuleiro
-    int tabuleiro[10][10] = {
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0}
-    };
+int main() {
 
-    //Posicionamento dos navios
+    // Navios (3 células cada)
+    int naviohori[3] = {3,3,3};     // Horizontal
+    int naviovert[3] = {3,3,3};     // Vertical
+    int naviodiag1[3] = {3,3,3};    // Diagonal 
+    int naviodiag2[3] = {3,3,3};    // Diagonal 
 
-    // Coordenadas escolhidas 
-    int linhaH = 4, colunaH = 2;  // Navio horizontal
-    int linhaV = 1, colunaV = 7;  // Navio vertical
+    // Tabuleiro 10x10 inicializado com água
+    int tabuleiro[10][10] = {0};
 
-    // Colocar navio horizontal
+    // Coordenadas dos navios
+    int linhaH = 4, colunaH = 2;  // Horizontal
+    int linhaV = 1, colunaV = 7;  // Vertical
+    int linhaD1 = 0, colunaD1 = 0;  // Diagonal 
+    int linhaD2 = 9, colunaD2 = 0;  // Diagonal 
+
+
+    // Posicionar navio horizontal
+
     for (int i = 0; i < 3; i++) {
         tabuleiro[linhaH][colunaH + i] = naviohori[i];
     }
 
-    // Colocar navio vertical
+  
+    // Posicionar navio vertical
+  
     for (int i = 0; i < 3; i++) {
         tabuleiro[linhaV + i][colunaV] = naviovert[i];
     }
 
-    //Impressão do tabuleiro
+  
+    // Posicionar navio diagonal 
+    
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linhaD1 + i][colunaD1 + i] = naviodiag1[i];
+    }
 
+   
+    // Posicionar navio diagonal 
+    
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linhaD2 - i][colunaD2 + i] = naviodiag2[i];
+    }
+
+    
+    // Impressão do tabuleiro
+    
     printf("\n     TABULEIRO\n");
     printf("   0 1 2 3 4 5 6 7 8 9\n");
 
     for (int i = 0; i < 10; i++) {
-        printf("%d  ", i);  // Número da linha
+        printf("%d  ", i);
         for (int j = 0; j < 10; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
